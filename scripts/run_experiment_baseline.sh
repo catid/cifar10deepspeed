e1a9f34f0f6982bf31b3693ff17c7fea88867c0b
catid <mrcatid@gmail.com>
2023-12-03 23:14:53 +0000

    script

---
+++ scripts/run_experiment_baseline.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# See models/model_loader.py for different architectures
+
+notes="run_experiment_baseline.sh"
+
+iterations=3
+
+# Function to launch training
+launch_training() {
+    # Loop to run the command multiple times
+    for ((i=1; i<=iterations; i++)); do
+        echo "Launching training (Iteration $i)..."
+        ./launch_local_train.sh --reset --name="${name}" --arch="${arch}" --params="${params}" --notes="${notes}"
+    done
+}
+
+name="vary_mlp_dim" # no spaces allowed
+arch="vit_tiny" # no spaces allowed
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=512" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=384" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=256" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=192" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=128" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=80" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=64" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=32" # no spaces allowed
+launch_training
+
+params="patch_size=4,dim=512,depth=4,heads=6,mlp_dim=16" # no spaces allowed
+launch_training
