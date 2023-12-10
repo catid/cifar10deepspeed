09f4bccccb9584aff0dc3e912023b445d972bf6f
catid <mrcatid@gmail.com>
2023-12-10 06:44:02 +0000

    exp

--- scripts/exp_x_transformers.sh
+++ scripts/exp_x_transformers.sh
@@ -37,36 +37,3 @@ params="dim=384,depth=4,heads=7" # no spaces allowed
 launch_training
 params="dim=384,depth=4,heads=6" # no spaces allowed
 launch_training
-
-params="dim=256,depth=6,heads=8" # no spaces allowed
-launch_training
-params="dim=256,depth=5,heads=8" # no spaces allowed
-launch_training
-params="dim=256,depth=5,heads=7" # no spaces allowed
-launch_training
-params="dim=256,depth=4,heads=7" # no spaces allowed
-launch_training
-params="dim=256,depth=4,heads=6" # no spaces allowed
-launch_training
-
-params="dim=192,depth=6,heads=8" # no spaces allowed
-launch_training
-params="dim=192,depth=5,heads=8" # no spaces allowed
-launch_training
-params="dim=192,depth=5,heads=7" # no spaces allowed
-launch_training
-params="dim=192,depth=4,heads=7" # no spaces allowed
-launch_training
-params="dim=192,depth=4,heads=6" # no spaces allowed
-launch_training
-
-params="dim=128,depth=6,heads=8" # no spaces allowed
-launch_training
-params="dim=128,depth=5,heads=8" # no spaces allowed
-launch_training
-params="dim=128,depth=5,heads=7" # no spaces allowed
-launch_training
-params="dim=128,depth=4,heads=7" # no spaces allowed
-launch_training
-params="dim=128,depth=4,heads=6" # no spaces allowed
-launch_training
